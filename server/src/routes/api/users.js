579b0c6557d5c6eb83093c89648c681800b82065
whuang214 <whuang2146@gmail.com>
2023-08-01 12:03:13 -0400

    initalized boilerplate code

---
+++ server/src/routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const usersCtrl = require("../../controllers/users");
+
+/*---------- Public Routes ----------*/
+router.post("/signup", usersCtrl.signup);
+router.post("/login", usersCtrl.login);
+
+/*---------- Protected Routes ----------*/
+
+module.exports = router;
+
+/*---------- Protected Routes ----------*/
---
+++ server/src/server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const favicon = require("serve-favicon");
+
+require("./config/database");
+
+// Require controllers here
+
+const app = express();
+
+// add in when the app is ready to be deployed
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(logger("dev"));
+app.use(express.json());
+
+// Configure the auth middleware
+// This decodes the jwt token, and assigns
+// the user information to req.user
+app.use(require("./config/auth"));
+// api routes must be before the "catch all" route
+app.use("/api/users", require("./routes/api/users"));
+
+// "catch all" route
+app.get("/*", function (req, res) {
+  // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send("hello world");
+});
+
+const port = process.env.PORT || 3001;
+
+const { PORT = 8000 } = process.env;
+app.listen(PORT, () => {
+  console.log();
+  console.log(`  App running in port ${PORT}`);
+  console.log();
+  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
+});
