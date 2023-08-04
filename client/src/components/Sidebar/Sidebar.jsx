5168f42b67d0cd73683b41cc2980fd9636144de3
whuang214 <whuang2146@gmail.com>
2023-08-04 10:15:43 -0400

    update sidebar styles

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,12 @@
 import styles from "./Sidebar.module.css";
+import { FaPlus } from "react-icons/fa";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <button className={styles.addChatButton}>Add New Chat</button>
+      <button className={styles.addChatButton}>
+        <FaPlus /> New Chat
+      </button>
     </div>
   );
 }
