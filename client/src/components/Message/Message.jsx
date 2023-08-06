11ce3a235e0fd89f6177f065e9d86b2b615cba0d
whuang214 <whuang2146@gmail.com>
2023-08-06 16:47:42 -0400

    fix overflow of chat

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -1,13 +1,25 @@
 import styles from "../Message/Message.module.css";
 
+// Placeholder avatars
+const userAvatar = "https://via.placeholder.com/30";
+const botAvatar = "https://via.placeholder.com/30";
+
 export default function Message({ message }) {
+  const sender = message.role;
+  const content = message.content;
+
   return (
     <div
-      className={`${styles.message} ${
-        message.sender === "user" ? styles.userMessage : styles.botMessage
+      className={`${styles.messageContainer} ${
+        sender === "user" ? styles.userMessage : styles.botMessageContainer
       }`}
     >
-      {message.content}
+      <img
+        src={sender === "user" ? userAvatar : botAvatar}
+        alt={`${sender}'s avatar`}
+        className={styles.avatar}
+      />
+      <span className={styles.message}>{content}</span>
     </div>
   );
 }
