36a52b49f6351f74fd786b5231582ceeed9dbee7
whuang214 <whuang2146@gmail.com>
2023-08-07 12:45:34 -0400

    add styles to sidebar popup

--- client/src/components/OptionsPopup/OptionsPopup.jsx
+++ client/src/components/OptionsPopup/OptionsPopup.jsx
@@ -1,5 +1,8 @@
 import styles from "./OptionsPopup.module.css";
 
+import { AiOutlineUser, AiOutlineClear } from "react-icons/ai";
+import { MdOutlineLogout } from "react-icons/md";
+
 export default function OptionsPopup({ isPopupOpen, setPopupOpen, onLogout }) {
   const handleOutsideClick = (event) => {
     // Check if the click is outside the container or the popup
@@ -13,10 +16,15 @@ export default function OptionsPopup({ isPopupOpen, setPopupOpen, onLogout }) {
       className={`${styles.popupMenu} ${isPopupOpen ? styles.active : ""}`}
       onClick={handleOutsideClick}
     >
-      <div className={styles.popupOption}>Edit User</div>
-      <div className={styles.popupOption}>Clear Chats</div>
+      <div className={styles.popupOption}>
+        <AiOutlineUser /> Edit User
+      </div>
+      <div className={`${styles.popupOption} ${styles.clearChatButton}`}>
+        <AiOutlineClear /> Clear Chats
+      </div>
       <div className={styles.seperator}></div>
       <div className={styles.popupOption} onClick={onLogout}>
+        <MdOutlineLogout />
         Logout
       </div>
     </div>
