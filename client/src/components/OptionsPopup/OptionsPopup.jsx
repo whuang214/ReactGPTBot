787236315172315314e4ffc94f79f1dae751c5bc
whuang214 <whuang2146@gmail.com>
2023-08-07 13:26:29 -0400

    add when clicked outside popup options componetn will close

--- client/src/components/OptionsPopup/OptionsPopup.jsx
+++ client/src/components/OptionsPopup/OptionsPopup.jsx
@@ -1,25 +1,66 @@
-import styles from "./OptionsPopup.module.css";
+import { useState, useEffect, useRef } from "react";
 
-import { AiOutlineUser, AiOutlineClear } from "react-icons/ai";
+import ConfirmationOverlay from "../ConfirmationOverlay/ConfirmationOverlay";
+
+import {
+  AiOutlineUser,
+  AiOutlineClear,
+  AiOutlineInfoCircle,
+} from "react-icons/ai";
 import { MdOutlineLogout } from "react-icons/md";
 
+import styles from "./OptionsPopup.module.css";
+
 export default function OptionsPopup({ isPopupOpen, setPopupOpen, onLogout }) {
-  const handleOutsideClick = (event) => {
-    // Check if the click is outside the container or the popup
-    if (!event.target.closest(`.${styles.userProfileContainer}`)) {
-      setPopupOpen(false);
+  const [isClearChatConfirmationOpen, setIsClearChatConfirmationOpen] =
+    useState(false);
+  const popupRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        setPopupOpen(false);
+      }
+    };
+
+    if (isPopupOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
     }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isPopupOpen]);
+
+  const toggleClearChatClick = () => {
+    setIsClearChatConfirmationOpen(!isClearChatConfirmationOpen);
+  };
+
+  const handleCancelClear = () => {
+    toggleClearChatClick();
+  };
+
+  const handleClearChats = () => {
+    // TODO: Clear chats
   };
 
   return (
     <div
+      ref={popupRef}
       className={`${styles.popupMenu} ${isPopupOpen ? styles.active : ""}`}
-      onClick={handleOutsideClick}
     >
+      <div className={styles.popupOption}>
+        <AiOutlineInfoCircle /> About
+      </div>
       <div className={styles.popupOption}>
         <AiOutlineUser /> Edit User
       </div>
-      <div className={`${styles.popupOption} ${styles.clearChatButton}`}>
+      <div
+        className={`${styles.popupOption} ${styles.clearChatButton}`}
+        onClick={toggleClearChatClick}
+      >
         <AiOutlineClear /> Clear Chats
       </div>
       <div className={styles.seperator}></div>
@@ -27,6 +68,14 @@ export default function OptionsPopup({ isPopupOpen, setPopupOpen, onLogout }) {
         <MdOutlineLogout />
         Logout
       </div>
+      {isClearChatConfirmationOpen && (
+        <ConfirmationOverlay
+          title="Clear Chats?"
+          prompt="Are you sure you want to clear all chats?"
+          onCancel={handleCancelClear}
+          onConfirm={handleClearChats}
+        />
+      )}
     </div>
   );
 }
